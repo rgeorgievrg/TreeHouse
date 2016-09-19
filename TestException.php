bdb4ba977985d3b94849e10ab9b196882248a4f5
radoslav.georgiev@demax.bg <dxgl1063>
2016-09-19 19:07:58 +0300

    home

---
+++ TestException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rado
+ * Date: 19.09.16
+ * Time: 14:15
+ */
+
+ini_set("display_errors", 1);
+
+//try {
+//    $ba = (2/0 );
+//} catch (Exception $e) {
+//    echo 'Caught exception: ', $e->getMessage(), "\n";
+//}
+
+echo PHP_RELEASE_VERSION;
